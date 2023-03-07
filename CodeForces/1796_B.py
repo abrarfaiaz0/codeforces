281796feb718c4a902bb20c568058ea4b5d44021
abrarfaiaz0 <abrarfaiaz0@gmail.com>
2023-03-07 21:28:14 +0600

    refactor experiment

--- CodeForces/1796_B.py
+++ CodeForces/1796_B.py
@@ -1,18 +1,22 @@
 t = int(input())
 for q in range(t):
+
     found = False
     index = 0
     a = input()
     b = input()
     if(a[0] == b[0]):
+
         print("YES")
         print(f"{a[0]}*")
         continue
     elif(a[len(a)-1] ==b[len(b)-1]):
+
         print("YES")
         print(f"*{a[len(a)-1]}")
         continue
     for i in range(len(a)):
+
         if found == True:
             break
         for j in range(len(b)):
@@ -22,8 +26,10 @@ for q in range(t):
                     index = i
                     break
     if found == False:
+
         print("NO")
     else:
+        
         print("YES")
         print(f"*{a[index:index+2]}*")
     
